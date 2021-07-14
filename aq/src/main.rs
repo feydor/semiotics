c76e21e7b61c02cef501216ea5a9ba78822b101c
Victor Reyes <vreyes85739@outlook.com>
2021-07-13 22:38:09 -0700

    Add hex trinome color printing to aq.

--- aq/src/main.rs
+++ aq/src/main.rs
@@ -3,6 +3,8 @@ use clap::{App, AppSettings, Arg};
 use lazy_static::lazy_static;
 use std::io;
 use std::io::Write;
+use colored::*;
+use std::convert::TryInto;
 const PROJECT_NAME: &str = "aq";
 const VERSION: &str = "0.1.0";
 const ABOUT: &str = "deCrypter for Anglobal communications";
@@ -85,6 +87,38 @@ fn print_results(buffer: &String) {
         print!(" -> {}", res);
     }
     print!("\n");
+
+    // println!("{:->width$} THE IRON LAW OF SIX {:->width$}", "", "", width=96/2);
+    let mut i = 0;
+    let mut trinomes = Vec::<u8>::new();
+    for c in buffer.chars().filter(|&c|c.is_alphanumeric()).collect::<String>().chars() {
+        let trinome: u8 = nummificate(&c.to_string())[0].try_into().unwrap();
+        trinomes.push(trinome);
+        i += 1;
+        if i % 3 == 0 {
+            print_hex_trinomes(&trinomes);
+            trinomes.clear();
+        }
+    }
+}
+
+// prints the hex trinome in color, using itself
+fn print_hex_trinomes(trinomes: &Vec<u8>) {
+    if trinomes.len() < 3 {
+        panic!("trinomes must be hex.");
+    }
+
+    let mut s = String::from(" ");
+    for t in trinomes {
+        s.push_str(format!("{:#04X} ", t).as_str());
+    }
+
+    // println!("--Hex Trinomes--");
+    const SCALE: u8 = 4;
+    for _ in 0..6 {
+        print!("{} ", s.on_truecolor(trinomes[0]*SCALE, trinomes[1]*SCALE, trinomes[2]*SCALE));
+    }
+    print!{"\n"};
 }
 
 // full digital-reduction of any query string using August Barrow's method of Anglossic Qabbala
