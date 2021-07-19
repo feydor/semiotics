0596a7c056eb0109851218b97e7cae9eae0267cf
Victor Reyes <vreyes85739@outlook.com>
2021-07-18 22:31:46 -0700

    Modify tetractys printout.

--- tetractys/src/main.rs
+++ tetractys/src/main.rs
@@ -1,23 +1,10 @@
 //! tetractys.rs - triangular number generation; method of binomial coefficients
 use std::convert::TryInto;
-// use lazy_static::lazy_static;
-/*
-lazy_static! {
-    static ref GRID: Vec<u8> = Vec::<u8>::with_capacity(100);
-}
-*/
 
 fn main() {
     let n: u32 = std::env::args().nth(1).expect("no number given").parse().expect("argument not a number");
-    let size: u32 = match n % 2 == 0 {
-         true => (n+1)*(n+1),
-         false => n*n
-    };
-
-    let grid: Vec<bool> = vec![false; size.try_into().unwrap()];
     let tetrads = tetrads(n);
     print_tetrads(&tetrads);
-    // print_grid(&grid);
     println!("The {}th triangular number is: {}", n, triangular(n));
 }
 
@@ -42,38 +29,34 @@ fn tri_upto(n: u32) -> Vec<u32> {
 // returns the nth triangular number
 // T=1,3,6,10,15,21,etc
 fn triangular(n: u32) -> u32 {
-    if n < 2 {
-        return 1; 
-    }
+    if n < 2 { return 1; }
     return n + triangular(n-1);
 }
 
 fn print_tetrads(tetrads: &Vec<u32>) {
     let base: u32 = *tetrads.last().unwrap();
+    // println!(" r Σ");
     for n in tetrads {
-        // first print spaces
+        // first print (base-n) spaces
+        print!("{:>2} ", *n);
         for _ in *n..=base {
-            print!(" ");
+            print!("·");
         }
 
-        for _ in 0..*n {
-            print!("{}", "· ");
+        // then print n dots
+        for i in 0..*n {
+            print!("●");
+
+            if i != *n-1 {
+                print!(" ");
+            }
+        }
+
+        // closing spaces
+        for _ in *n..=base {
+            print!("·");
         }
         print!("\n");
     }
 }
 
-fn print_grid(grid: &Vec<bool>) {
-    let dim = (grid.len() as f64).sqrt();
-    println!("{:?}", dim);
-    for i in 0..grid.len() {
-        if i as f64 % dim == 0.0 {
-            print!("\n");
-        }
-
-        match grid[i] {
-            true => print!("•"),
-            false => print!("·")
-        }
-    }
-}
