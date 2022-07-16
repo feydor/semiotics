98c6f549be6a8e426d25e2067bee604fd4114f98
feydor <vreyes85739@outlook.com>
2022-07-16 15:38:50 -0500

    fix permutations

--- ana/build.sh
+++ ana/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
-FLAGS='-Wall -Wextra -g'
+FLAGS='-Wall -Wextra -Wconversion -pedantic -g'
 cc ../src/main.c -o ana $FLAGS -lm
--- ana/src/main.c
+++ ana/src/main.c
@@ -4,9 +4,21 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-unsigned fact(unsigned n) {
+size_t fact(size_t n) {
     if (!n) return 1;
     else return n * fact(n-1);
+}
+
+void swap(char *a, char *b) {
+    char tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void reverse(char *word, size_t len) {
+    for (size_t i = 0; i < len; ++i) {
+        swap(word + len-i-1, word + i);
+    }
 }
 
 // a string with everything but the char at i
@@ -29,91 +41,137 @@ char *rest(char word[static 1], size_t i, size_t len) {
 }
 
 // shifts the char at i to i+1, wrapping if necessary
-char *shr(char *word, size_t i, size_t len) {
+void shr(char *word, size_t i, size_t len) {
     assert(i < len);
-    char *res = strdup(word);
-    char ch = *(word+i);
 
     if (i+1 == len) {
         // rotate right
-        for (size_t i = 1; i < len; ++i) {
-            *(res + i) = *(word + i -1);
+        char *res = strdup(word);
+        char *last = word + i;
+        for (size_t j = 1; j < len; ++j) {
+            *(res + j) = *(word + j - 1);
         }
-        *res = ch;
+        *res = *last;
+        strcpy(word, res);
+        free(res);
     } else {
-        // swap
-        *(res + i) = *(word + i + 1);
-        *(res + i + 1) = ch;
+        swap(word+i, word+i+1);
     }
-
-    return res;
 }
 
-// swap word[i] with word[j]
-char *swapch(char *word, size_t i, size_t j, size_t len) {
-    assert(i < len && j < len);
+char *reversed(char *word, size_t len) {
     char *res = strdup(word);
-    char tmp = word[i];
-    res[i] = word[j];
-    res[j] = tmp;
+    for (size_t i = 0; i < len; ++i) {
+        res[len-i-1] = word[i];
+    }
     return res;
 }
 
+typedef struct Dict Dict;
+struct Dict {
+    char **strings;
+    size_t size;
+    size_t capacity;
+};
 
+Dict *dict_create() {
+    Dict *dict = malloc(sizeof(Dict));
+    if (!dict) return NULL;
+    dict->size = 0;
+    dict->capacity = 5;
+    dict->strings = calloc(dict->capacity, sizeof(char *));
+    if (!dict->strings) return NULL;
+    for (size_t i = 0; i < dict->capacity; ++i) {
+        dict->strings[i] = strdup("\0");
+        if (!dict->strings[i]) return NULL;
+    }
+    return dict;
+}
 
-size_t dict_index = 0;
-size_t dict_capacity = 0;
+void dict_push(Dict *dict, char word[]) {
+    if (dict->size == dict->capacity) {
+        dict->capacity *= 2;
+        dict->strings = realloc(dict->strings, dict->capacity * sizeof(char *));
+        if (!dict->strings) {
+            fprintf(stderr, "dict_push: failed realloc\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    dict->strings[dict->size] = strdup(word);
+    if (!dict->strings[dict->size]) {
+        fprintf(stderr, "dict_push: failed strdup\n");
+        exit(EXIT_FAILURE);
+    }
+    ++dict->size;
+}
 
-bool dict_includes(char *dict[], char word[]) {
-    assert(dict[0]);
-    for (size_t i = 0; i < dict_index; ++i) {
-        if (!strcmp(dict[i], word)) {
+bool dict_includes(Dict *dict, char word[]) {
+    for (size_t i = 0; i < dict->size; ++i) {
+        if (!strcmp(dict->strings[i], word)) {
             return true;
         }
     }
     return false;
 }
 
-// assumes dict is preallocated to hold N words of known length
-void dict_add(char *dict[], char word[]) {
-    dict[dict_index] = strdup(word);
-    ++dict_index;
+void dict_print(Dict *dict) {
+    for (size_t i = 0; i < dict->size; ++i) {
+        printf("%s\n", dict->strings[i]);
+    }
 }
 
-bool dict_full() {
-    return dict_index == dict_capacity;
+void dict_free(Dict *dict) {
+    for (size_t i = 0; i < dict->size; ++i) {
+        free(dict->strings[i]);
+        dict->strings[i] = NULL;
+    }
+    free(dict->strings);
+    dict->strings = NULL;
 }
 
-void swap_all(char *anagrams[], char *word, size_t len) {
-    if (dict_full()) return;
+bool str_includes_ch(char *str, size_t len, char ch) {
     for (size_t i = 0; i < len; ++i) {
-        for (size_t j = i+1; j < len; ++j) {
-            char *gram = swapch(word, i, j, len);
-            
-            if (!dict_includes(anagrams, gram)) {
-                dict_add(anagrams, gram);
-                swap_all(anagrams, gram, len);
-            }
+        if (str[i] == ch) return true;
+    }
+    return false;
+}
+
+size_t unique_letters(char *word, size_t len) {
+    size_t unique = 0;
+    for (size_t i = 0; i < len; ++i) {
+        char *rest_word = rest(word, i, len);
+        unique += !str_includes_ch(rest_word, len-1, word[i]);
+    }
+    return unique;
+}
+
+void permutate(Dict *dict, char word[], size_t curr, size_t end) {
+    if (curr == end) {
+        if (!dict_includes(dict, word))
+            dict_push(dict, word);
+    } else {
+        for (size_t i = curr; i <= end; ++i) {
+            swap(word+curr, word+i);
+            permutate(dict, word, curr+1, end);
+            swap(word+curr, word+i);
         }
     }
 }
 
 int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+    
     // there are n! permutations for a word of len n
-    char *word = "abcd";
+    char word[] = "mother";
     size_t len = strlen(word);
-    size_t n = fact(len);
-    char *permutations[n];
-    dict_capacity = n;
-    for (size_t i = 0; i < n; ++i) {
-        permutations[i] = word;
-    }
+    printf("# of unique letters: %lu\n", unique_letters(word, len));
+    Dict *permutations = dict_create();
 
-    swap_all(permutations, word, len);
-
-    printf("printing permutations...\n");
-    printf("# of permutations: %lu\n", n);
-    for (size_t i = 0; i < n; ++i) {
-        printf("%s\n", permutations[i]);
-    }
+    permutate(permutations, word, 0, len-1);
+    
+    printf("printing unique permutations...\n");
+    printf("# of unique permutations: %lu\n", permutations->size);
+    dict_print(permutations);
+    dict_free(permutations);
 }
