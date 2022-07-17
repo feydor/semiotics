11a2622fd05a00af131d35284bead70fbc660270
feydor <vreyes85739@outlook.com>
2022-07-17 16:07:11 -0500

    remove uneccesary clones

--- ana/src/main.rs
+++ ana/src/main.rs
@@ -1,10 +1,7 @@
 use cursive::{
-    event::{Event},
+    event::Event,
     traits::*,
-    views::{
-        Dialog, EditView, LinearLayout, ScrollView, SelectView,
-        TextView,
-    },
+    views::{Dialog, EditView, LinearLayout, ScrollView, SelectView, TextView},
     Cursive,
 };
 use log::LevelFilter;
@@ -19,7 +16,7 @@ fn main() {
     simple_logging::log_to_file("test.log", LevelFilter::Info).expect("simple_logging failed");
     let args: Vec<String> = std::env::args().collect();
     match args.len() {
-        2 => {
+        x if x > 1 => {
             if args[1] == "-h" || args[1] == "--help" {
                 usage_and_exit();
             }
@@ -27,10 +24,10 @@ fn main() {
         _ => usage_and_exit(),
     }
 
-    let input: String = args[1].trim().to_string();
+    let input: &str = args[1].trim();
     let mut siv = cursive::default();
     siv.set_theme(cursive::theme::Theme::default().with(|theme| {
-        use cursive::theme::{BaseColor::*, Color::*, PaletteColor::*, BorderStyle::*};
+        use cursive::theme::{BaseColor::*, Color::*, PaletteColor::*};
         theme.shadow = false;
         theme.palette[Background] = TerminalDefault;
         theme.palette[Primary] = Dark(Black);
@@ -44,8 +41,15 @@ fn main() {
 
     siv.add_global_callback(Event::Key(cursive::event::Key::Esc), |s| s.quit());
 
-    let input_box = EditView::new().on_submit(handle_submit).with_name("input_box").full_width();
-    let results = SelectView::<String>::new().item(input.clone(), input.clone()).with_name("results").full_height();
+    let input = input.to_string();
+    let input_box = EditView::new()
+        .on_submit(handle_submit)
+        .with_name("input_box")
+        .full_width();
+    let results = SelectView::<String>::new()
+        .item(&input, input.clone())
+        .with_name("results")
+        .full_height();
     let scroll_results = ScrollView::new(results);
 
     siv.add_layer(
@@ -70,19 +74,17 @@ fn main() {
     siv.run();
 }
 
-fn siv_popup(siv: &mut Cursive, msg: &str) {
-    siv.add_layer(Dialog::text(msg).button("Ret", |s| {s.pop_layer();}));
-}
-
 fn handle_submit(siv: &mut Cursive, text: &str) {
     let mut results_list = siv.find_name::<SelectView<String>>("results").unwrap();
     let input = results_list.selection().unwrap();
     let lowercase = input.to_ascii_lowercase();
     let sentence: Vec<&str> = lowercase.split(' ').collect();
     let letters: Vec<char> = text.chars().collect();
-    let result: String =
-        ana::gram::remove_letters_from_sentence(&sentence[0..], &letters).join(" ").to_ascii_uppercase();
+    let result: String = ana::gram::remove_letters_from_sentence(&sentence[0..], &letters)
+        .join(" ")
+        .to_ascii_uppercase();
+    // TODO: show each letter of text in a different color, if its letters were used in the sentence
     if result != lowercase {
-        results_list.add_item(result.clone(), result);
+        results_list.add_item(text.to_string() + " -> " + &result, result);
     }
 }
