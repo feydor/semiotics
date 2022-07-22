eeb139bfe3b37f4ee85c40965268a63517071e43
feydor <vreyes85739@outlook.com>
2022-07-21 19:55:06 -0500

    shorten strview to strv, spartan language and all

--- wgolf/wgolf.c
+++ wgolf/wgolf.c
@@ -20,14 +20,14 @@ struct dict {
     size_t size;
 };
 
-typedef struct strview strview;
-struct strview {
+typedef struct strv strv;
+struct strv {
     size_t ptr;
     size_t len;
 };
 
 // potential buffer overflow
-strview strpool_new(strpool *pool, char *s) {
+strv strpool_new(strpool *pool, char *s) {
     size_t saved = pool->sp;
     size_t len = 0;
     while (*s) {
@@ -42,26 +42,26 @@ strview strpool_new(strpool *pool, char *s) {
         fprintf(stderr, "strpool buffer overflow\n");
         exit(EXIT_FAILURE);
     } 
-    
+
     pool->size++;
-    return (strview){ .ptr = saved, .len = len };
+    return (strv){ .ptr = saved, .len = len };
 }
 
-char *strpool_get(strpool *pool, strview s) {
+char *strpool_get(strpool *pool, strv s) {
     return pool->buf+s.ptr;
 }
 
-void strview_print_debug(strpool *pool, strview s) {
+void strv_print_debug(strpool *pool, strv s) {
     printf("{.ptr=%ld, .len=%ld} ", s.ptr, s.len);
     size_t sp = s.ptr;
     while (pool->buf[sp]) {
-       printf("'%c'", pool->buf[sp]);
-       sp++;
+        printf("'%c'", pool->buf[sp]);
+        sp++;
     }
     printf("\n");
 }
 
-int strview_cmp(strpool *pool, strview s1, strview s2) {
+int strv_cmp(strpool *pool, strv s1, strv s2) {
     size_t sp1 = s1.ptr;
     size_t sp2 = s2.ptr;
     int a = 0, b = 0;
@@ -77,60 +77,60 @@ int strview_cmp(strpool *pool, strview s1, strview s2) {
     return !(a-b) ? 0 : a-b > 0 ? 1 : -1;
 }
 
-void test_strview_cmp(strpool *p) {
-    strview a = strpool_new(p, "canoe");
-    strview b = strpool_new(p, "canon");
-    assert(-1 == strview_cmp(p, a, b));
+void test_strv_cmp(strpool *p) {
+    strv a = strpool_new(p, "canoe");
+    strv b = strpool_new(p, "canon");
+    assert(-1 == strv_cmp(p, a, b));
 
     a = strpool_new(p, "");
     b = strpool_new(p, "a");
-    assert(-1 == strview_cmp(p, a, b));
+    assert(-1 == strv_cmp(p, a, b));
 
     a = strpool_new(p, "A");
     b = strpool_new(p, "a");
-    assert(-1 == strview_cmp(p, a, b));
-    
+    assert(-1 == strv_cmp(p, a, b));
+
     a = strpool_new(p, "cil");
-        b = strpool_new(p, "cml");
-    assert(-1 == strview_cmp(p, a, b));
+    b = strpool_new(p, "cml");
+    assert(-1 == strv_cmp(p, a, b));
 }
 
 // f('lewis',3) -> 'lews'
-void strview_delete_at(strpool *p, strview s, size_t i) {
+void strv_delete_at(strpool *p, strv s, size_t i) {
     assert(i < s.len);
     memcpy(strpool_get(p,s)+i, strpool_get(p,s)+i+1, s.len-i+1);
     s.len--;
 }
 
-void test_strview_delete_at(strpool *p) {
+void test_strv_delete_at(strpool *p) {
     // delete in middle
-    strview a = strpool_new(p, "lewis");
-    strview_delete_at(p, a, 3);
+    strv a = strpool_new(p, "lewis");
+    strv_delete_at(p, a, 3);
     assert(0 == strcmp("lews", strpool_get(p, a)));
 
     // delete at end
     a = strpool_new(p, "abc");
-    strview_delete_at(p, a, 2);     
+    strv_delete_at(p, a, 2);
     assert(0 == strcmp("ab", strpool_get(p, a)));
-    
+
     // delete at the begining
     a = strpool_new(p, "ed");
-    strview_delete_at(p, a, 0);     
+    strv_delete_at(p, a, 0);
     assert(0 == strcmp("d", strpool_get(p, a)));
 }
 
-void strview_replace_at(strpool *p, strview s, size_t i, char c) {
+void strv_replace_at(strpool *p, strv s, size_t i, char c) {
     assert(i < s.len);
     *(strpool_get(p, s)+i) = c;
 }
 
-void test_strview_replace_at(strpool *p) {
-    strview a = strpool_new(p, "repl");
-    strview_replace_at(p, a, 2, 'a');
+void test_strv_replace_at(strpool *p) {
+    strv a = strpool_new(p, "repl");
+    strv_replace_at(p, a, 2, 'a');
     assert(!strcmp("real", strpool_get(p, a)));
 }
 
-strview strview_prepend(strpool *p, strview s, char c) {
+strv strv_prepend(strpool *p, strv s, char c) {
     // cheat by making new str in pool
     char new[s.len+1];
     new[0] = c;
@@ -138,14 +138,15 @@ strview strview_prepend(strpool *p, strview s, char c) {
     return strpool_new(p, new);
 }
 
-void test_strview_prepend(strpool *p) {
-    strview a = strpool_new(p, "end");
-    strview res = strview_prepend(p, a, 'r');
+void test_strv_prepend(strpool *p) {
+    strv a = strpool_new(p, "end");
+    strv res = strv_prepend(p, a, 'r');
     assert(!strcmp("rend", strpool_get(p, res)));
     assert(!strcmp("end", strpool_get(p, a)));
 }
 
-strview strview_cpymov(strpool *p, strview dest, strview src) {
+// TODO: what is the point of this?
+strv strv_cpymov(strpool *p, strv dest, strv src) {
     if (dest.len == src.len) {
         strcpy(strpool_get(p, dest), strpool_get(p, src));
         dest.len = src.len;
@@ -153,72 +154,72 @@ strview strview_cpymov(strpool *p, strview dest, strview src) {
         memcpy(strpool_get(p, dest), strpool_get(p, src), src.len+1);
         dest.len = src.len;
     } else {
-        strview new = strpool_new(p, strpool_get(p, src));
+        strv new = strpool_new(p, strpool_get(p, src));
         dest.ptr = new.ptr;
         dest.len = new.len;
     }
     return dest;
 }
-void test_strview_cpymov(strpool *p) {
+void test_strv_cpymov(strpool *p) {
     // x.len == y.len
-    strview x = strpool_new(p, "tweedledee");
-    strview y = strpool_new(p, "tweedledum");
-    strview res = strview_cpymov(p, y, x);
+    strv x = strpool_new(p, "tweedledee");
+    strv y = strpool_new(p, "tweedledum");
+    strv res = strv_cpymov(p, y, x);
     assert(!strcmp("tweedledee", strpool_get(p, res)));
 
     // x.len < y.len
     x = strpool_new(p, "rel");
     y = strpool_new(p, "relo");
-    res = strview_cpymov(p, y, x);
+    res = strv_cpymov(p, y, x);
     assert(!strcmp("rel", strpool_get(p, res)));
-    
+
     // x.len > y.len
     x = strpool_new(p, "relo");
     y = strpool_new(p, "rel");
-    res = strview_cpymov(p, y, x);
+    res = strv_cpymov(p, y, x);
     assert(!strcmp("relo", strpool_get(p, res)));
 }
 
 
 // TODO: invalidates src
-void strview_move(strpool *p, strview *dest, strview *src) {
+void strv_move(strpool *p, strv *dest, strv *src) {
     dest->ptr = src->ptr;
     dest->len = src->len;
     // TODO: Let the strpool know that src is "freed"
 }
 
-void test_strview_move(strpool *p) {
-    strview dest = strpool_new(p, "tweedledee");
-    strview src = strpool_new(p, "tweedledum");
-    strview_move(p, &dest, &src);
+void test_strv_move(strpool *p) {
+    strv dest = strpool_new(p, "tweedledee");
+    strv src = strpool_new(p, "tweedledum");
+    strv_move(p, &dest, &src);
     assert(!strcmp("tweedledum", strpool_get(p, dest)));
 }
 
 // f(s1: "dipple", s2: "dapple") -> s1: "dippledapple", s2: "dapple"
-void strview_cat(strpool *p, strview *s1, strview *s2) {
+void strv_cat(strpool *p, strv *s1, strv *s2) {
     char temp[s1->len + s2->len+1];
     memcpy(temp, strpool_get(p, *s1), s1->len);
     memcpy(temp+s1->len, strpool_get(p, *s2), s2->len+1);
-    strview svt = strpool_new(p, temp);
-    strview_move(p, s1, &svt);
+    strv svt = strpool_new(p, temp);
+    strv_move(p, s1, &svt);
 }
 
-void test_strview_cat(strpool *p) {
-    strview x = strpool_new(p, "con");
-    strview y = strpool_new(p, "cat");
-    strview_cat(p, &x, &y);
+void test_strv_cat(strpool *p) {
+    strv x = strpool_new(p, "con");
+    strv y = strpool_new(p, "cat");
+    strv_cat(p, &x, &y);
     assert(!strcmp("concat", strpool_get(p, x)));
     x = strpool_new(p, "");
     y = strpool_new(p, "cat");
-    strview_cat(p, &x, &y);
+    strv_cat(p, &x, &y);
     assert(!strcmp("cat", strpool_get(p, x)));
     x = strpool_new(p, "cat");
     y = strpool_new(p, "");
-    strview_cat(p, &x, &y);
+    strv_cat(p, &x, &y);
     assert(!strcmp("cat", strpool_get(p, x)));
     x = strpool_new(p, "");
     y = strpool_new(p, "");
-    strview_cat(p, &x, &y);
+    strv_cat(p, &x, &y);
     assert(!strcmp("", strpool_get(p, x)));
 }
 
@@ -245,42 +246,39 @@ dict load_dict(size_t wordlen) {
 }
 
 char ALPHABET[] = "abcdefghijklmnopqrstuvwxyz";
-/*void wgolf(strpool sp, dict d, strview dest, strview src, size_t start, size_t *n) {
-    if (!strview_cmp(dest, src)) return;
-    int ac = 0;
-    for (char a = ALPHABET[ac]; a; ++ac) {
-        for (int i = 0; i < src.len; ++i) {
-            strview_delete_at(sp, src, i);
-            strview word = strview_prependc(sp, s, dest.ptr);
-            if (in_dict(strpool_get(sp, word))) {
-                src = strview_move(src, word);
-                *n++;
-            }
-        }
-    }
+/*void wgolf(strpool sp, dict d, strv dest, strv src, size_t start, size_t *n) {
+  if (!strv_cmp(dest, src)) return;
+  int ac = 0;
+  for (char a = ALPHABET[ac]; a; ++ac) {
+  for (int i = 0; i < src.len; ++i) {
+  strv_delete_at(sp, src, i);
+  strv word = strv_prependc(sp, s, dest.ptr);
+  if (in_dict(strpool_get(sp, word))) {
+  src = strv_move(src, word);
+ *n++;
+ }
+ }
+ }
 
-    if (strview_cmp(dest, src))
-        wgolf(dest, src, start, n);
-}
-*/
+ if (strv_cmp(dest, src))
+ wgolf(dest, src, start, n);
+ }
+ */
 
-void test_strview() {
+void test_strv() {
     strpool p = {0};
-    test_strview_cmp(&p);
-    test_strview_delete_at(&p);
-    test_strview_replace_at(&p);
-    test_strview_prepend(&p);
-    test_strview_move(&p);
-    test_strview_cat(&p);
-    printf("all tests passed on strview\n");
+    test_strv_cmp(&p);
+    test_strv_delete_at(&p);
+    test_strv_replace_at(&p);
+    test_strv_prepend(&p);
+    test_strv_move(&p);
+    test_strv_cat(&p);
+    printf("all tests passed on strv\n");
 }
 
 int main(void) {
-    strpool sp = {0};
-    strview s = strpool_new(&sp, "wgolf 0.1.0");
-    printf("%s\n", strpool_get(&sp, s));
-    test_strview();
-
+    printf("wgolf 0.1.0\n");
+    test_strv();
     dict d = load_dict(4);
     assert(27 == sizeof(ALPHABET));
 }
