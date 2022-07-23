d2993892bfd1a97f4fe662e3ec7dc3bff9755537
feydor <vreyes85739@outlook.com>
2022-07-23 18:15:20 -0500

    integrate older version with color hex trinome printing

--- aq/src/lib.rs
+++ aq/src/lib.rs
@@ -40,31 +40,6 @@ fn is_single_digit(n: &i32) -> bool {
     n.abs() < 10
 }
 
-// every three chars in query generates an RGB hex values
-// ignores chars at end, where i % 3 == 0
-// src: https://northanger.livejournal.com/272673.html
-pub fn hex_triplets(query: &str) -> Vec<(u8, u8, u8)> {
-    let mut hex_trips = Vec::<(u8, u8, u8)>::new();
-    let mut i = 0;
-    let mut arr: [u8; 3] = [0, 0, 0];
-    for ch in query.chars() {
-        arr[i] = nummificate(&ch.to_string().to_uppercase())[0] as u8;
-        i += 1;
-        if i % 3 == 0 {
-            hex_trips.push((arr[0], arr[1], arr[2]));
-            i = 0;
-        }
-    }
-    hex_trips
-}
-
-pub fn hex_trinome(query: &str) -> (u8, u8, u8) {
-    let n = nummificate(&query.to_uppercase())[0];
-    ((n & 0x00FF0000 >> 16) as u8,
-    (n & 0x0000FF00 >> 8) as u8,
-    n as u8)
-}
-
 #[cfg(test)]
 mod tests {
 	use super::*;
--- aq/src/main.rs
+++ aq/src/main.rs
@@ -1,7 +1,10 @@
-/* aq.rs - gematric and digital reduction functions for Anglossic Qabbala (AQ) */
-use toei::toei::*;
-use clap::{App, Arg};
-
+/* aw - interactive AQ nummifier */
+use clap::{App, AppSettings, Arg};
+use std::io;
+use std::io::Write;
+use colored::*;
+use std::convert::TryInto;
+use libaq;
 const PROJECT_NAME: &str = "aq";
 const VERSION: &str = "0.1.0";
 const ABOUT: &str = "deCrypter for Anglobal communications";
@@ -10,47 +13,58 @@ fn main() {
     let args = App::new(PROJECT_NAME)
         .version(VERSION)
         .about(ABOUT)
+        .setting(AppSettings::ArgRequiredElseHelp)
         .arg(
             Arg::with_name("QUERY")
                 .help("an alphanumeric-encoded string")
                 .index(1),
         )
         .arg(
-            Arg::with_name("t")
-                .short("t")
+            Arg::with_name("i")
+                .short("i")
                 .multiple(false)
-                .help("print hex trinomes"),
+                .help("start interactive prompt"),
         )
         .get_matches();
 
-    let query = sanitize_query(args.value_of("QUERY").unwrap_or_default());
-
-    let aq_closure = |s: &str| {
-        let v = aq::nummificate(&s.to_uppercase());
-        let mut out: String = format!("{} -> {}", s, v[0]);
-        if args.is_present("t") {
-            out += "\n";
-            let rgb = aq::hex_trinome(&s);
-            out += &format!("{:#08X} ", v[0]);
-            out += &color::bg_rgb("        ", rgb);
-        }
-        return out;
+    let query: String = match args.value_of("QUERY") {
+        None => String::new(),
+        Some(query) => sanitize_query(&query),
     };
 
-    if args.is_present("QUERY") {
-        println!("{}", aq_closure(&query));
+    if args.is_present("i") {
+        start_prompt(&query)
+    } else {
+        print_results(&query);
     }
+}
 
-    let mut t = Toei::new();
-    println!("{} {}", PROJECT_NAME, VERSION);
+fn start_prompt(initial: &str) {
+    println!("{}\n{}", PROJECT_NAME, VERSION);
+    let mut buffer = match initial.is_empty() {
+        true => String::new(),
+        false => String::from(initial),
+    };
+    let stdin = io::stdin();
+
     loop {
-        match t.run(aq_closure) {
-            ToeiResult::Done(x) => t.print_result(&x),
-            ToeiResult::Quit => break,
-            _ => (),
+        if !buffer.is_empty() {
+            print_results(&buffer);
         }
-    }
+        buffer.clear();
 
+        print! {"> "};
+        io::stdout().flush().unwrap();
+        stdin
+            .read_line(&mut buffer)
+            .expect("error: unable to read user input");
+        buffer = buffer.trim().to_uppercase();
+        if buffer.is_empty() || is_quit(&buffer) {
+            break;
+        }
+        print_results(&buffer);
+        buffer.clear();
+    }
 }
 
 // removes non-alphanumerics and converts to uppercase
@@ -62,3 +76,50 @@ fn sanitize_query(q: &str) -> String {
         .to_uppercase();
 }
 
+fn print_results(buffer: &String) {
+    print!("{}", buffer);
+    for res in &libaq::nummificate(&sanitize_query(&buffer)) {
+        print!(" -> {}", res);
+    }
+    print!("\n");
+
+    println!("{:->width$} THE IRON LAW OF SIX {:->width$}", "", "", width=40);
+    let mut i = 0;
+    let mut trinomes = Vec::<u8>::new();
+    for c in buffer.chars().filter(|&c|c.is_alphanumeric()).collect::<String>().chars() {
+        let trinome: u8 = libaq::nummificate(&c.to_string())[0].try_into().unwrap();
+        trinomes.push(trinome);
+        i += 1;
+        if i % 3 == 0 {
+            print_hex_trinomes(&trinomes);
+            trinomes.clear();
+        }
+    }
+}
+
+// prints the hex trinome in color, using itself
+fn print_hex_trinomes(trinomes: &Vec<u8>) {
+    if trinomes.len() < 3 {
+        panic!("trinomes must be hex.");
+    }
+
+    let mut s = String::from(" ");
+    for t in trinomes {
+        s.push_str(format!("{:#04X} ", t).as_str());
+    }
+
+    // println!("--Hex Trinomes--");
+    const SCALE: u8 = 4;
+    for _ in 0..6 {
+        print!("{} ", s.on_truecolor(trinomes[0]*SCALE, trinomes[1]*SCALE, trinomes[2]*SCALE));
+    }
+    print!{"\n"};
+}
+
+fn is_quit(q: &str) -> bool {
+    match q {
+        "q" => true,
+        "Q" => true,
+        _ => false,
+    }
+}
